87203f3e803d8a99922a445cbdb1f96cccc1cdd0
farhandotme <princeofficial355@gmail.com>
2024-12-28 22:56:45 +0530

    message

--- server/controllers/checkPassword.js
+++ server/controllers/checkPassword.js
@@ -1,22 +1,17 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
 async function checkPassword(req, res) {
   try {
     const { password, userId } = req.body;
-
     const user = await userModel.findById(userId);
-
     const verifyPassword = await bcrypt.compare(password, user.password);
-
     if (!verifyPassword) {
       return res.status(400).json({
         message: "Please check password",
         error: true,
       });
     }
-
     const tokenData = {
       id: user._id,
       email: user.email,
@@ -24,12 +19,10 @@ async function checkPassword(req, res) {
     const token = jwt.sign(tokenData, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
-
     const cookieOptions = {
       http: true,
       secure: true,
     };
-
     return res.cookie("token", token, cookieOptions).status(200).json({
       message: "Login successfully",
       token: token,
@@ -42,5 +35,4 @@ async function checkPassword(req, res) {
     });
   }
 }
-
 module.exports = checkPassword;
